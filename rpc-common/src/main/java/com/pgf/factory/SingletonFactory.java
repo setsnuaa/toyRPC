0854efd4ca4bdcf7c794637700b40ba9ce0a50b0
setsnuaa <1051338166@qq.com>
2022-05-19 20:35:24 +0800

    添加单例模式工厂类

---
+++ rpc-common/src/main/java/com/pgf/factory/SingletonFactory.java
@@ -0,0 +1,37 @@
+package com.pgf.factory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @name:
+ * @author:pan.gefei
+ * @date:2022/5/19 15:53
+ * @description: 获取单例对象的工厂类
+ */
+public final class SingletonFactory {
+    private static final Map<String, Object> OBJECT_MAP = new ConcurrentHashMap<>();
+
+    private SingletonFactory() {
+    }
+
+    public static <T> T getInstance(Class<T> clazz) {
+        if (clazz == null) {
+            throw new IllegalArgumentException();
+        }
+        String key = clazz.toString();
+        if (OBJECT_MAP.containsKey(key)) {
+            return clazz.cast(OBJECT_MAP.get(key));
+        } else {
+            return clazz.cast(OBJECT_MAP.computeIfAbsent(key, k -> {
+                try {
+                    return clazz.getDeclaredConstructor().newInstance();
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                         NoSuchMethodException e) {
+                    throw new RuntimeException(e.getMessage(), e);
+                }
+            }));
+        }
+    }
+}
