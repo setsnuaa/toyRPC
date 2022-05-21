945bb990bc8ba2533fd4204cd5803c7ccff5f8bd
setsnuaa <1051338166@qq.com>
2022-05-21 19:34:56 +0800

    添加服务提供者

--- rpc-core/src/main/java/com/pgf/provider/ServiceProvider.java
+++ rpc-core/src/main/java/com/pgf/provider/ServiceProvider.java
@@ -1,10 +1,19 @@
 package com.pgf.provider;
 
+import com.pgf.config.RpcServiceConfig;
+
 /**
  * @name:
  * @author:pan.gefei
  * @date:2022/4/28 21:09
- * @description:
+ * @description: 提供方法调用的服务器调用这个接口的方法来发布服务
  */
 public interface ServiceProvider {
+
+    void addService(RpcServiceConfig rpcServiceConfig);
+
+    Object getService(String rpcServiceName);
+
+    void publishService(RpcServiceConfig rpcServiceConfig);
+
 }
