c65af56df7a35f385c80c351aefe6e187fea9f4e
setsnuaa <1051338166@qq.com>
2022-06-07 16:00:37 +0800

    zk

---
+++ rpc-core/src/main/java/com/pgf/registry/zk/util/CuratorUtils.java
@@ -0,0 +1,129 @@
+package com.pgf.registry.zk.util;
+
+import com.pgf.enums.RpcConfigEnum;
+import com.pgf.utils.PropertiesFileUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.imps.CuratorFrameworkState;
+import org.apache.curator.framework.recipes.cache.PathChildrenCache;
+import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @name:
+ * @author:pan.gefei
+ * @date:2022/5/21 16:52
+ * @description:
+ */
+@Slf4j
+public class CuratorUtils {
+
+    private static final int BASE_SLEEP_TIME = 1000;
+    private static final int MAX_RETRIES = 3;
+    public static final String ZK_REGISTER_ROOT_PATH = "/my-rpc";
+    private static final Map<String, List<String>> SERVICE_ADDRESS_MAP = new ConcurrentHashMap<>();
+    private static final Set<String> REGISTERED_PATH_SET = ConcurrentHashMap.newKeySet();
+    private static CuratorFramework zkClient;
+    private static final String DEFAULT_ZOOKEEPER_ADDRESS = "127.0.0.1:2181";
+
+    private CuratorUtils() {
+    }
+
+    public static void createPersistentNode(CuratorFramework zkClient, String path) {
+        try {
+            if (REGISTERED_PATH_SET.contains(path) || zkClient.checkExists().forPath(path) != null) {
+                log.info("Node already exists. The node is:[{}]", path);
+            } else {
+                // path = /my-rpc/com.pgf.HelloService/127.0.0.1:9999
+                zkClient.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(path);
+                log.info("Node created successfully. The node is:[{}]", path);
+            }
+            REGISTERED_PATH_SET.add(path);
+        } catch (Exception e) {
+            log.error("create persistent node for path [{}] fail", path);
+        }
+    }
+
+    public static List<String> getChildrenNodes(CuratorFramework zkClient, String rpcServiceName) {
+        if (SERVICE_ADDRESS_MAP.containsKey(rpcServiceName)) {
+            return SERVICE_ADDRESS_MAP.get(rpcServiceName);
+        }
+        List<String> result = null;
+        String servicePath = ZK_REGISTER_ROOT_PATH + "/" + rpcServiceName;
+        try {
+            result = zkClient.getChildren().forPath(servicePath);
+            SERVICE_ADDRESS_MAP.put(rpcServiceName, result);
+            registerWatcher(zkClient, rpcServiceName);
+        } catch (Exception e) {
+            log.error("get children nodes for path [{}] fail", servicePath);
+        }
+        return result;
+    }
+
+    public static void clearRegistry(CuratorFramework zkClient, InetSocketAddress inetSocketAddress) {
+        REGISTERED_PATH_SET.parallelStream().forEach(p -> {
+            try {
+                // 把所有节点名后缀是服务器IP地址的节点都删了
+                // 节点命名的时候是
+                if (p.endsWith(inetSocketAddress.toString())) {
+                    zkClient.delete().forPath(p);
+                }
+            } catch (Exception e) {
+                log.error("clear registry for path [{}] fail", p);
+            }
+        });
+        log.info("All registered services on the server are cleared:[{}]", REGISTERED_PATH_SET);
+    }
+
+    public static CuratorFramework getZkClient() {
+        Properties properties = PropertiesFileUtil.readPropertiesFile(RpcConfigEnum.RPC_CONFIG_PATH.getPropertyValue());
+        String zookeeperAddress = properties != null && properties.getProperty(RpcConfigEnum.ZK_ADDRESS.getPropertyValue()) != null ? properties.getProperty(RpcConfigEnum.ZK_ADDRESS.getPropertyValue()) : DEFAULT_ZOOKEEPER_ADDRESS;
+        // 如果已经启动，直接返回
+        if (zkClient != null && zkClient.getState() == CuratorFrameworkState.STARTED) {
+            return zkClient;
+        }
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(BASE_SLEEP_TIME, MAX_RETRIES);
+        zkClient = CuratorFrameworkFactory.builder()
+                .connectString(zookeeperAddress)
+                .retryPolicy(retryPolicy)
+                .build();
+        zkClient.start();
+        try {
+            if (!zkClient.blockUntilConnected(30, TimeUnit.SECONDS)) {
+                throw new RuntimeException("Time out waiting to connect to ZK!");
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return zkClient;
+    }
+
+    /**
+     * 监听子节点变化，每次变化都会触发回调函数
+     *
+     * @param zkClient
+     * @param rpcServiceName
+     * @throws Exception
+     */
+    private static void registerWatcher(CuratorFramework zkClient, String rpcServiceName) throws Exception {
+        String servicePath = ZK_REGISTER_ROOT_PATH + "/" + rpcServiceName;
+        PathChildrenCache pathChildrenCache = new PathChildrenCache(zkClient, servicePath, true);
+        PathChildrenCacheListener pathChildrenCacheListener = (curatorFramework, pathChildrenCacheEvent) -> {
+            List<String> serviceAddresses = curatorFramework.getChildren().forPath(servicePath);
+            SERVICE_ADDRESS_MAP.put(rpcServiceName, serviceAddresses);
+        };
+        pathChildrenCache.getListenable().addListener(pathChildrenCacheListener);
+        pathChildrenCache.start();
+    }
+}
