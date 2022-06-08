51682ad7b79654a8fd83755117051bbe15465982
setsnuaa <1051338166@qq.com>
2022-06-08 15:51:26 +0800

    服务注册

---
+++ rpc-core/src/main/java/com/pgf/registry/zk/ZkServiceRegistryImpl.java
@@ -0,0 +1,24 @@
+package com.pgf.registry.zk;
+
+import com.pgf.registry.ServiceRegistry;
+import com.pgf.registry.zk.util.CuratorUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.framework.CuratorFramework;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author pan.gefei
+ * @name
+ * @date 2022/6/8 15:03
+ * @description
+ */
+@Slf4j
+public class ZkServiceRegistryImpl implements ServiceRegistry {
+    @Override
+    public void registerService(String rpcServiceName, InetSocketAddress inetSocketAddress) {
+        String servicePath = CuratorUtils.ZK_REGISTER_ROOT_PATH + "/" + rpcServiceName + "/" + inetSocketAddress.toString();
+        CuratorFramework zkClient = CuratorUtils.getZkClient();
+        CuratorUtils.createPersistentNode(zkClient, servicePath);
+    }
+}
